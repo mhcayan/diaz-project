58d9ffb8494bc7b1667c13e0388c306e06e27c00
Monazil Chowdhury <mchow15@lsu.edu>
2020-09-23 18:21:02 -0500

    fix task1. column name and index moved to enum class

--- calc.py
+++ calc.py
@@ -1,14 +1,26 @@
 #pip install xlrd
 #pip install openpyxl
+#pip install matplotlib
 import pandas as pd
 import datetime
 import numpy as np
+import os.path
+import enum
+from openpyxl import load_workbook
 
-#todo: merge the constants in an Enum class
+class ExcelColumnName(enum.Enum):
+    DATE_TIME = 'Date/Time'
+    TIME_DIFF_SEC = 'TIME DIFF SEC'
+    USER_FULL_NAME = 'User full name'
+    EVENT_CONTEXT = 'Event context'
 
-FILE_DIR = "E:\\code\\diaz-project\\resources"
-FILE_NAME = "mybook.xlsx"
-FILE_PATH = FILE_DIR + "\\" + FILE_NAME
+class ExcelColumnIndex(enum.Enum):
+    TIME_DIFF_SEC = 4
+
+FILE_DIR = r'E:\code\student-data-project\resources'
+OUTPUT_FILE_DIR = FILE_DIR
+FILE_NAME = 'mybook.xlsx'
+FILE_PATH = os.path.join(FILE_DIR, FILE_NAME)
 
 '''
 password = "1g2K47B"
@@ -26,8 +38,6 @@ xlwb.Save()
 xlwb.Close()
 xlApp.Quit()
 '''
-from openpyxl import load_workbook
-
 def write_df_to_excel(file_path = FILE_PATH, df = None, sheet_name = 'sheet', index_bool = False):
     book = load_workbook(file_path)
     writer = pd.ExcelWriter(file_path, engine='openpyxl') # pylint: disable=abstract-class-instantiated
@@ -37,44 +47,45 @@ def write_df_to_excel(file_path = FILE_PATH, df = None, sheet_name = 'sheet', in
     writer.save()
     writer.close()
 
-TIME_DIFF_SEC_COLUMN = 'TIME DIFF SEC'
+def write_df_to_csv(file_path, df):
+    df.to_csv(file_path, encoding = 'utf-8')
 
-def task1(file_path, input_sheet_name, output_sheet_name):
+def task1(file_path, input_sheet_name, output_file_path):
+
     df = pd.read_excel(file_path, sheet_name = input_sheet_name)
+    
     print(df.head())
 
     previous_event_context = None
     end_time_index = None
     start_time_index = None
     DATE_TIME_FORMAT = '%m/%d/%y %H:%M:%S'
-    DATE_TIME_COLUMN = 'Date/Time'
     
-    TIME_DIFF_COLUMN_INDEX = 4
     ll = []
 
-    #todo: replace iterrow with faster alternative
-    for index, row in df.iterrows():
-        if(row['Event context'] != previous_event_context):
+    for index in df.index:
+        if(df.at[index,ExcelColumnName.EVENT_CONTEXT.value] != previous_event_context):
             if(start_time_index != None) :
-                start_time = datetime.datetime.strptime(df.at[start_time_index, DATE_TIME_COLUMN], DATE_TIME_FORMAT)
-                end_time = datetime.datetime.strptime(df.at[end_time_index, DATE_TIME_COLUMN], DATE_TIME_FORMAT)
+                start_time = datetime.datetime.strptime(df.at[start_time_index, ExcelColumnName.DATE_TIME.value], DATE_TIME_FORMAT)
+                end_time = datetime.datetime.strptime(df.at[end_time_index, ExcelColumnName.DATE_TIME.value], DATE_TIME_FORMAT)
                 #print("end_time_index = %r start_time_index = %r " % (end_time_index, start_time_index))
                 #print("end_time = %r start_time = %r" % (end_time, start_time))
+                if((start_time_index != end_time_index and start_time == end_time) and end_time_index - 1 >= 0): # consecutive events with same timestamp and next event exists
+                    end_time = datetime.datetime.strptime(df.at[end_time_index - 1, ExcelColumnName.DATE_TIME.value], DATE_TIME_FORMAT) # end_time = next_event's start_time
+
                 diff_time = end_time - start_time
                 active_row = df.iloc[start_time_index]
                 active_row_as_list = active_row.values.tolist()
-                active_row_as_list[TIME_DIFF_COLUMN_INDEX] = diff_time.seconds
-                #print(active_row_as_list)
+                active_row_as_list[ExcelColumnIndex.TIME_DIFF_SEC.value] = diff_time.seconds
                 ll.append(active_row_as_list)
-                #print(diff_time.seconds)
             end_time_index = index
         start_time_index = index
-        previous_event_context = row['Event context']
+        previous_event_context = df.at[index, ExcelColumnName.EVENT_CONTEXT.value]
 
     df1 = pd.DataFrame(ll, columns = df.columns)
-    write_df_to_excel(file_path = file_path, df = df1, sheet_name = output_sheet_name)
-
-    print("step 1 done...")
+    #write_df_to_excel(file_path = file_path, df = df1, sheet_name = output_sheet_name)
+    write_df_to_csv(file_path = output_file_path, df = df1)
+    print("task 1 done...")
 
 def task2_and_3(file_path, input_sheet_name, output_sheet_name):
     df = pd.read_excel(file_path, sheet_name=input_sheet_name)
@@ -82,7 +93,7 @@ def task2_and_3(file_path, input_sheet_name, output_sheet_name):
     consecutive_zero_dict = dict()
     #total_consecutive_zero = 0
     #total_sec_assigned_to_consecutive_zero = 0
-    df[TIME_DIFF_SEC_COLUMN] = df[TIME_DIFF_SEC_COLUMN].astype(float)
+    df[ExcelColumnName.TIME_DIFF_SEC.value] = df[ExcelColumnName.TIME_DIFF_SEC.value].astype(float)
     EVENT_CONTEXT_COLUMN = 'Event context'
     for index in df.index:
         if df.at[index, TIME_DIFF_SEC_COLUMN] == 0:
@@ -96,8 +107,8 @@ def task2_and_3(file_path, input_sheet_name, output_sheet_name):
                 #total_consecutive_zero = total_consecutive_zero + zero_count
                 #total_sec_assigned_to_consecutive_zero = total_sec_assigned_to_consecutive_zero + 60
                 for i in range(start_index, index):
-                    df.at[i, TIME_DIFF_SEC_COLUMN] = time_value
-                    event_name = df.at[i, EVENT_CONTEXT_COLUMN]
+                    df.at[i, ExcelColumnName.TIME_DIFF_SEC.value] = time_value
+                    event_name = df.at[i, ExcelColumnName.EVENT_CONTEXT.value]
                     if event_name in consecutive_zero_dict:
                         data = consecutive_zero_dict.get(event_name)
                         data[0] = data[0] + 1 #increment counter
@@ -117,31 +128,44 @@ def task2_and_3(file_path, input_sheet_name, output_sheet_name):
 #drop the first row of each students consecutive data. (Except the first row in the excel)
 #ques: first row in the excel?
 def task4(file_path, input_sheet_name, output_sheet_name):
-    USER_FULL_NAME_COLUMN = 'User full name'
     df = pd.read_excel(file_path, sheet_name=input_sheet_name)
-    df.drop(df[(df[USER_FULL_NAME_COLUMN] != df[USER_FULL_NAME_COLUMN].shift(1)) & \
-        (df[USER_FULL_NAME_COLUMN].shift(1).apply(lambda x : isinstance(x,str)))].index, inplace = True)
+    df.drop(df[(df[ExcelColumnName.USER_FULL_NAME.value] != df[ExcelColumnName.USER_FULL_NAME.value].shift(1)) & \
+        (df[ExcelColumnName.USER_FULL_NAME.value].shift(1).apply(lambda x : isinstance(x,str)))].index, inplace = True)
     write_df_to_excel(file_path, df, output_sheet_name)
     print("task4 done...")
 
+def get_cut_off_points(df, event_name):
+    return np.percentile(df[df[ExcelColumnName.EVENT_CONTEXT.value] == event_name][ExcelColumnName.TIME_DIFF_SEC.value], [25,75], interpolation='midpoint')
+
 def task5(file_path, input_sheet_name, output_sheet_name):
     df = pd.read_excel(file_path, sheet_name=input_sheet_name)
-    EVENT_CONTEXT_COLUMN = 'Event context'
-    TIME_DIFF_SEC_COLUMN = 'TIME DIFF SEC'
+    EVENT_CONTEXT_COLUMN = ExcelColumnName.EVENT_CONTEXT.value
+    TIME_DIFF_SEC_COLUMN = ExcelColumnName.TIME_DIFF_SEC.value
+    event_list = sorted(df[EVENT_CONTEXT_COLUMN].unique())
     event_dict = dict()
-    for event_name in sorted(df[EVENT_CONTEXT_COLUMN].unique()):
+    for event_name in event_list:
         event_dict[event_name] = df[df[EVENT_CONTEXT_COLUMN] == event_name][TIME_DIFF_SEC_COLUMN].describe()
     stat_df = pd.DataFrame.from_dict(event_dict, orient = 'index')
     stat_df = stat_df.rename_axis(None)
     stat_df.index.names = ['']
     write_df_to_excel(file_path, stat_df, input_sheet_name + "-stat", index_bool=True)
-    print(stat_df.head())
+    print(len(df))
+    """
+    for event_name in event_list:
+        df[df[EVENT_CONTEXT_COLUMN] == event_name][TIME_DIFF_SEC_COLUMN].hist()
+        if cnt:
+            break
+    """
+    for event_name in event_list:
+        cut_off_points = get_cut_off_points(df, event_name)
+        df.drop(df[(df[EVENT_CONTEXT_COLUMN] == event_name) & \
+            ((df[TIME_DIFF_SEC_COLUMN] < cut_off_points[0]) | (df[TIME_DIFF_SEC_COLUMN] > cut_off_points[1]))].index, \
+                inplace = True)
+    print(len(df))
     print("task 5 done")
 
 if __name__ == "__main__":
-    #task1(FILE_PATH, input_sheet_name='Sheet1', output_sheet_name='Task1')
+    task1(FILE_PATH, input_sheet_name='Sheet1', output_file_path = os.path.join(OUTPUT_FILE_DIR, 'Task1.csv'))
     #task2_and_3(FILE_PATH, input_sheet_name='Task1', output_sheet_name = 'Task2-3')
     #task4(FILE_PATH, input_sheet_name='Task2-3', output_sheet_name='Task4')
-    task5(FILE_PATH, input_sheet_name='Task4', output_sheet_name=None)
-    
-
+    #task5(FILE_PATH, input_sheet_name='Task4', output_sheet_name=None)
